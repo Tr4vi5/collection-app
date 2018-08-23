ee4a0229977c11d15f79cdd38d85318fcbbf82cd
Tr4vi5 <trav.dunn@outlook.com>
2018-08-23 11:26:02 -0500

    views and angular routes added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
 const myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeController as hc'
+    }).when('/movies',{
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesController as mc'
+    }).when('/movie-genres',{
+        templateUrl: 'views/movie-genres.html',
+        controller: 'MovieGenresController as mgc'
+    }).otherwise({
+        templateUrl: 'views/404.html'
+    });
+});//end angular routes config
