f154aa2249ab835f5374d20119c4d6e407f85bda
Tr4vi5 <trav.dunn@outlook.com>
2018-08-23 14:49:49 -0500

    movie genres appending to form on DOM and inputs clearing after POST

--- server/public/scripts/controllers/movies-controller.js
+++ server/public/scripts/controllers/movies-controller.js
@@ -2,7 +2,6 @@ myApp.controller('MoviesController',['$http', function($http){
     console.log('In Movies Controller');
     const self = this;
 
-
     self.getGenres = function(){
         $http({
             method: 'GET',
@@ -19,13 +18,13 @@ myApp.controller('MoviesController',['$http', function($http){
     self.addMovie = function(movie){
         self.movieToAdd = movie;
         console.log('About to add', movie);  
-
         $http({
             method: 'POST',
             url: '/movies',
             data: self.movieToAdd
         }).then(function(response){
             console.log('POSTing to server:', response.data);
+            self.movieIn = {};
         }).catch(function(error){
             console.log('Error in POST to server', error);
         });
