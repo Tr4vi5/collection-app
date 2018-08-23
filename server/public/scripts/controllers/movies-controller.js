016907d8ad8a1aff4433058741a7da162e39e31b
Tr4vi5 <trav.dunn@outlook.com>
2018-08-23 12:44:47 -0500

    html form added to movies view

--- server/public/scripts/controllers/movies-controller.js
+++ server/public/scripts/controllers/movies-controller.js
@@ -1,4 +1,12 @@
 myApp.controller('MoviesController',['$http', function($http){
     console.log('In Movies Controller');
     const self = this;
+
+    self.movieToAdd={};
+
+    self.addMovie = function(movie){
+        self.movieToAdd = movie;
+        console.log('About to add', movie);  
+    };
+
 }]);
